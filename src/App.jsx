430e881c70c0ae27d510ba35d0f4f88244dda1c4
Frank <frankielee1971@gmail.com>
2026-04-11 15:33:52 -0400

    Add Coming Soon route

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import CONFIG from "./config";
 import DashboardPage from "./pages/DashboardPage";
 import LandingPage from "./pages/LandingPage";
+import ComingSoon from "./pages/comingsoon";
 
 const App = () => (
   <Routes>
     <Route path={CONFIG.routes.landing} element={<LandingPage />} />
     <Route path={CONFIG.routes.dashboard} element={<DashboardPage />} />
+    <Route path="/coming-soon" element={<ComingSoon />} />
     <Route path="*" element={<Navigate to={CONFIG.routes.landing} replace />} />
   </Routes>
 );
 
 export default App;
+
