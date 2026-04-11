bb790adce276f6ab96c8e7d759a24d8502e92038
Frank <frankielee1971@gmail.com>
2026-04-10 20:06:46 -0400

    Fix firebaseAdmin import spelling

--- api/drive/list.ts
+++ api/drive/list.ts
@@ -1,6 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { google } from 'googleapis';
-import { getGoogleAuth } from '../_utils/googleAuth';
+import { getGoogleAuth } from '../_utils/googleAuth.js';
+
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
